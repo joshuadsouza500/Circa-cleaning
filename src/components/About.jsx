c763d530d0f2968d98a350e0a58b4d2987c7b99c
Joshua Dsouza <118182542+joshuadsouza500@users.noreply.github.com>
2025-02-24 22:57:49 +0300

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
     <section id='About' className="bg-[#E6DEDE8]" >
       <h2 className='text-5xl text-center font-bold py-5 my-3'>About Us</h2>
       <div className='flex flex-col-reverse sm:flex-row justify-center items-center gap-8'>
-      <p className='text-2xl text-slate-800 m-2 max-w-md'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      <p className='text-2xl text-[#A57C94] m-2 max-w-md'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
      <img className=' w-1/2 sm:w-1/3 ' src='/src/assets/aboutus.png' alt='about us image'></img>
      </div>
      <hr className='bg-black mx-auto w-1/2 my-20'></hr>
